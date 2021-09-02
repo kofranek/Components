6de57cbb147f6a559582fd937d81a2a54856ee9b
Jiří Kofránek <kofranek@gmail.com>
2021-09-02 20:58:18 +0200

    after 7

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,20 +1,20 @@
 import React, { useState } from 'react'
 
 const Counter = () => {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(1)
 
   const formCount = () => {
     return count === 0 ? 'Nula' : count
   }
 
+  const getBadgerclasses = () => {
+    let classes = 'badge m-2 bg-'
+    classes += count === 0 ? 'danger' : 'primary'
+    return classes
+  }
   return (
     <>
-      <span
-        style={{ fontSize: '15px', fontWeight: 'bold' }}
-        className='badge bg-primary m-2'
-      >
-        {formCount()}
-      </span>
+      <span className={getBadgerclasses()}>{formCount()}</span>
       <button className='btn btn-secondary btn-sm'>Increment</button>
     </>
   )
