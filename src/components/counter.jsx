7f5f8d9ef8ed57dc062110a824d2424bd04f5a45
Jiří Kofránek <kofranek@gmail.com>
2021-09-02 23:40:19 +0200

    Added decrement button, and decrement handler. If count = 0 hidden = true.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,10 +4,8 @@ const Counter = () => {
   const [count, setCount] = useState(0)
 
   const formCount = () => {
-    return count === 0 ? 'Nula' : count
+    return count === 0 ? 'Ноль' : count
   }
-
-  const tags = ['tag1', 'tag2', 'tag3']
 
   const getBadgerclasses = () => {
     let classes = 'badge m-2 bg-'
@@ -15,27 +13,35 @@ const Counter = () => {
     return classes
   }
 
-  const renderTags = () => {
-
-    if (tags.length === 0) return "no item"
-    else {
-      return tags.map(tag => <li key={tag}>{tag}</li>)
-    }
-    
+  const handleIncrement = productId => {
+    //console.log('increment')
+    setCount(count + 1)
+    console.log('productId=', productId)
   }
 
-  const handleIncrement=(productId)=>{
-      //console.log('increment')
-      setCount(count + 1)
-      console.log('productId=', productId, 'count=', count)
+  const handleDecrement = productId => {
+    //console.log('increment')
+    if (count>0) setCount(count - 1)
+    console.log('productId=', productId)
   }
 
+  let buttonVisibility = "true"
   return (
     <>
-      {tags.length ===0 && "you need to enter item! "}
-      {renderTags()}
       <span className={getBadgerclasses()}>{formCount()}</span>
-      <button  onClick = {()=>handleIncrement({id: 1})} className='btn btn-secondary btn-sm'>Increment</button>
+      <button
+        onClick={() => handleIncrement({ id: 1 })}
+        className='btn btn-secondary btn-sm'
+      >
+        Increment
+      </button>
+      <button
+        hidden = {count===0}
+        onClick={() => handleDecrement({ id: 1 })}
+        className='btn btn-secondary btn-sm m-2'
+      >
+        Decrement
+      </button>
     </>
   )
 }
